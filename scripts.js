d9f41a4a03e27a1f09c2b1cdc80492e3fcad9ac5
Brayan <brayan.sanchez.leon@correounivalle.edu.co>
2022-12-25 21:13:49 -0500

    Improved the style of the html

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 const botonEncriptar = document.getElementById("btn-encriptar");
 const botonDesencriptar = document.getElementById("btn-desencriptar");
 const botonCopiar = document.getElementById("btn-copiar");
-const formTextoRespuesta = document.querySelector(".form-texto-respuesta");
+const contenedorTextoRespuesta = document.querySelector(".container__mensaje");
+const contenedorIlustracion = document.querySelector(".container__ilustracion");
 const textoIngresado = document.getElementById("texto-ingresado");
 const textoRespuesta = document.getElementById("texto-respuesta");
 const mensajeError = document.getElementById("mensaje-error");
-const avisoSinTexto = document.querySelector(".container__ilustracion");
 const letras = ["e", "i", "a", "o", "u"];
 const letrasEncriptadas = ["enter", "imes", "ai", "ober", "ufat"];
 const letrasRegex = new RegExp("e|i|a|o|u", "g");
@@ -13,12 +13,17 @@ const letrasEncriptadasRegex = new RegExp("enter|imes|ai|ober|ufat", "g");
 let fraseConvertida = "";
 let clipboardIsAvailable = false;
 
+document.addEventListener("click", (e) => {
+  if (e.target.matches("#btn-encriptar")) {
+    encriptar(true, letrasRegex);
+  } else if (e.target.matches("#btn-desencriptar")) {
+    encriptar(false, letrasEncriptadasRegex);
+  }
+});
+
 navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
   if (result.state === "granted" || result.state === "prompt") {
     clipboardIsAvailable = true;
-    console.log("Clipboard API is available");
-  } else {
-    console.log("Clipboard API is not available");
   }
 });
 
@@ -38,18 +43,23 @@ function encriptar(encrypt, regex) {
   fraseConvertida = "";
   let value = textoIngresado.value;
 
-  if (encrypt) {
-    fraseConvertida = value.replace(regex, (match) => {
-      return letrasEncriptadas[letras.indexOf(match)];
-    });
-  } else {
-    fraseConvertida = value.replace(regex, (match) => {
-      return letras[letrasEncriptadas.indexOf(match)];
-    });
-  }
+  if (value !== "") {
+    if (encrypt) {
+      fraseConvertida = value.replace(regex, (match) => {
+        return letrasEncriptadas[letras.indexOf(match)];
+      });
+    } else {
+      fraseConvertida = value.replace(regex, (match) => {
+        return letras[letrasEncriptadas.indexOf(match)];
+      });
+    }
 
-  textoRespuesta.value = fraseConvertida;
-  textoIngresado.value = "";
+    textoRespuesta.value = fraseConvertida;
+    textoIngresado.value = "";
+    enableResponseTextInput();
+  } else {
+    enableResponseTextInput(false);
+  }
 }
 
 function changeButtonStatus(disable) {
@@ -68,21 +78,15 @@ function changeButtonStatus(disable) {
   }
 }
 
-function enableResponseTextInput() {
-  avisoSinTexto.classList.add("none");
-  formTextoRespuesta.classList.remove("none");
-  botonCopiar.classList.remove("none");
-}
-
-document.addEventListener("click", (e) => {
-  if (e.target.matches("#btn-encriptar")) {
-    encriptar(true, letrasRegex);
-    enableResponseTextInput();
-  } else if (e.target.matches("#btn-desencriptar")) {
-    encriptar(false, letrasEncriptadasRegex);
-    enableResponseTextInput();
+function enableResponseTextInput(enable = true) {
+  if (enable) {
+    contenedorIlustracion.classList.add("none");
+    contenedorTextoRespuesta.classList.remove("none");
+  } else {
+    contenedorIlustracion.classList.remove("none");
+    contenedorTextoRespuesta.classList.add("none");
   }
-});
+}
 
 textoIngresado.addEventListener("keyup", () => {
   const pattern = textoIngresado.dataset.pattern;
